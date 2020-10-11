d299dd8a50a9ba5d9a46fb772bf8c1a1aa55f98d
deepduke <runningwater.jc@gmail.com>
2020-10-12 01:02:01 +0800

    :sparkles: Supplement Chapter-5

---
+++ Chapter-5/ICP_traj/build/CMakeFiles/ICP_traj.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/deepduke/Documents/slam/Chapter-5/ICP_traj/src/ICP_traj.cpp" "/home/deepduke/Documents/slam/Chapter-5/ICP_traj/build/CMakeFiles/ICP_traj.dir/src/ICP_traj.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Chapter-5/ICP_traj/build/CMakeFiles/ICP_traj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ICP_traj.dir/src/ICP_traj.cpp.o"
+  "ICP_traj.pdb"
+  "ICP_traj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ICP_traj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
